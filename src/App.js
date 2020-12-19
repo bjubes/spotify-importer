bab31dfef3c9eaf70a57dce7086ff1a9bdf5beaa
Brian Jubelirer <brian2386@gmail.com>
2020-12-18 23:46:12 -0500

    spotify api response

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Home />
+        </Route>
+        <Route path="/callback">
+          <Callback />
+        </Route>
+      </Switch>
+    </Router>
   );
+}
+
+function Home() {
+  const spotifyClientID = "23eb37f53ae043208f66d85a3d7080c1"
+  const redirectUrl = encodeURIComponent("http://localhost:3000/callback")
+  const scope = "user-read-private%20user-read-email%20playlist-modify-public%20playlist-modify-private"
+  const handleSpotifyLogin = () => {
+    var url = "https://accounts.spotify.com/authorize?"
+    url += `client_id=${spotifyClientID}&`
+    url += `redirect_uri=${redirectUrl}&`
+    url += `scope=${scope}&`
+    url += "response_type=token"
+    console.log(url)
+    window.open(url)
+  }
+  
+  return (
+   <div className="App">
+  <Button variant="primary" onClick={handleSpotifyLogin}>Login with Spotify</Button>
+</div>
+  )
+}
+
+function Callback() {
+  const hash_fragment = window.location.hash
+  const params = new URLSearchParams(hash_fragment.substring(1))
+  const token = params.get('access_token')
+  const tokenType = params.get('token_type')
+  const expiresIn = params.get('expires_in')
+  const state = params.get('state')  
+  return (
+    <div>
+      <p>token: {token}</p>
+      <p>expires in: {expiresIn} seconds</p>
+    </div>
+  )
 }
 
 export default App;
